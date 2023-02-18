2868c4083bbb1e28014cc45c0fcf5bf14f260ed4
Rayen Oueslati <rayenoueslati60@gmail.com>
2023-02-18 15:04:34 +0100

    ✨ added useState hook

--- src/components/calculator.jsx
+++ src/components/calculator.jsx
@@ -1,32 +1,46 @@
+import React from 'react';
 import './calculator.css';
+//parent function component
+export function Calculator() {
 
-export function Calculator(){
-    return(
+    //useState hook
+    //the first paramater is to return the state the second is a function to modify the state
+    const [display, setDisplay] = React.useState(0);
+
+    function handleNumber(value) {
+        setDisplay(value);
+    }
+    function handleOperator(value) {
+
+    }
+    return (
         <div className="calculator">
-            <div className="display">0</div>
-            <CalcButton value="7"/>
-            <CalcButton value="8"/>
-            <CalcButton value="9"/>
-            <CalcButton className="operator" value="/"/>
+            <div className="display">{display}</div>
+            <CalcButton value="7" onClick={handleNumber} />
+            <CalcButton value="8" onClick={handleNumber} />
+            <CalcButton value="9" onClick={handleNumber} />
+            <CalcButton className="operator" value="/" onClick={handleOperator} />
 
-            <CalcButton value="4"/>
-            <CalcButton value="5"/>
-            <CalcButton value="6"/>
-            <CalcButton className="operator" value="*"/>
+            <CalcButton value="4" onClick={handleNumber} />
+            <CalcButton value="5" onClick={handleNumber} />
+            <CalcButton value="6" onClick={handleNumber} />
+            <CalcButton className="operator" value="*" onClick={handleOperator} />
 
-            <CalcButton value="1"/>
-            <CalcButton value="2"/>
-            <CalcButton value="3"/>
-            <CalcButton className="operator" value="-"/>
+            <CalcButton value="1" onClick={handleNumber} />
+            <CalcButton value="2" onClick={handleNumber} />
+            <CalcButton value="3" onClick={handleNumber} />
+            <CalcButton className="operator" value="-" onClick={handleOperator} />
 
-            <CalcButton value="C"/>
-            <CalcButton value="0"/>
-            <CalcButton value="="/>
-            <CalcButton className="operator" value="+"/>
+            <CalcButton value="C" />
+            <CalcButton value="0" onClick={handleNumber} />
+            <CalcButton value="=" />
+            <CalcButton className="operator" value="+" onClick={handleOperator} />
         </div>
     );
 }
 
-export function CalcButton(props){
-    return <button className={props.className}>{props.value}</button>
+//child function component
+export function CalcButton(props) {
+    //props.onClick define props callback function
+    return <button className={props.className} onClick={() => props.onClick(props.value)}>{props.value}</button>
 }
