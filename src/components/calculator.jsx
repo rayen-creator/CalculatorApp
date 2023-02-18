2f8f09d4474cde4a613f816e8ad3dff1902e0947
Rayen Oueslati <rayenoueslati60@gmail.com>
2023-02-18 16:06:55 +0100

    ✨ added function to make the calculator work

--- src/components/calculator.jsx
+++ src/components/calculator.jsx
@@ -5,17 +5,82 @@ export function Calculator() {
 
     //useState hook
     //the first paramater is to return the state the second is a function to modify the state
-    const [display, setDisplay] = React.useState(0);
+    const [calc, setCalc] = React.useState({
+        current: "0",
+        total: "0",
+        isInitial: true,
+        preOp: ""
+    });
 
     function handleNumber(value) {
-        setDisplay(value);
+        let newValue = value;
+        console.log("dd", calc.isInitial);
+        if (!calc.isInitial) {
+            newValue = calc.current + value;
+
+        }
+        setCalc({
+            current: newValue,
+            total: calc.total,
+            isInitial: false,
+            preOp:calc.preOp
+        });
     }
     function handleOperator(value) {
+        const total = doCalculation();
+
+        setCalc({
+            current: total.toString(),
+            total: total.toString(),
+            isInitial: true,
+            preOp: value
+        });
 
     }
+    function doCalculation() {
+        let total = parseInt(calc.total);
+        // debugger;
+        console.log(calc);
+        switch (calc.preOp) {
+            case "+":
+                total += parseInt(calc.current);
+                break;
+            case "-":
+                total -= parseInt(calc.current);
+
+                break;
+            case "*":
+                total *= parseInt(calc.current);
+
+                break;
+            case "/":
+                total /= parseInt(calc.current);
+                break;
+            default:
+                total = parseInt(calc.current);
+        }
+        return total;
+
+
+    }
+
+    function renderDisplay() {
+        return calc.current + calc.preOp;
+    }
+
+    function handleClear() {
+        setCalc({
+            current: "0",
+            total: "0",
+            isInitial: true,
+            preOp: ""
+        });
+    }
+
+  
     return (
         <div className="calculator">
-            <div className="display">{display}</div>
+            <div className="display">{renderDisplay()}</div>
             <CalcButton value="7" onClick={handleNumber} />
             <CalcButton value="8" onClick={handleNumber} />
             <CalcButton value="9" onClick={handleNumber} />
@@ -31,9 +96,9 @@ export function Calculator() {
             <CalcButton value="3" onClick={handleNumber} />
             <CalcButton className="operator" value="-" onClick={handleOperator} />
 
-            <CalcButton value="C" />
+            <CalcButton value="C" onClick={handleClear} />
             <CalcButton value="0" onClick={handleNumber} />
-            <CalcButton value="=" />
+            <CalcButton value="=" onClick={handleOperator} />
             <CalcButton className="operator" value="+" onClick={handleOperator} />
         </div>
     );
