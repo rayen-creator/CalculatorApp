207ac6d59bc6d95920d9300a7e2d518a35594073
Rayen Oueslati <rayenoueslati60@gmail.com>
2023-02-18 14:12:05 +0100

    🚀 initial commit

---
+++ src/components/calculator.jsx
@@ -0,0 +1,10 @@
+export function Calculator(){
+    return(
+        <div>
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+
+        </div>
+    );
+}
