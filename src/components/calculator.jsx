b7955c001392be07148482f4e028013ce6ec7bb8
Rayen Oueslati <rayenoueslati60@gmail.com>
2023-02-18 14:38:58 +0100

    ✅ update calculator function component

--- src/components/calculator.jsx
+++ src/components/calculator.jsx
@@ -1,26 +1,32 @@
+import './calculator.css';
+
 export function Calculator(){
     return(
         <div className="calculator">
+            <div className="display">0</div>
             <CalcButton value="7"/>
             <CalcButton value="8"/>
             <CalcButton value="9"/>
-            <CalcButton value="/"/>
+            <CalcButton className="operator" value="/"/>
 
             <CalcButton value="4"/>
             <CalcButton value="5"/>
             <CalcButton value="6"/>
-            <CalcButton value="*"/>
+            <CalcButton className="operator" value="*"/>
 
             <CalcButton value="1"/>
             <CalcButton value="2"/>
             <CalcButton value="3"/>
-            <CalcButton value="-"/>
+            <CalcButton className="operator" value="-"/>
 
-
+            <CalcButton value="C"/>
+            <CalcButton value="0"/>
+            <CalcButton value="="/>
+            <CalcButton className="operator" value="+"/>
         </div>
     );
 }
 
 export function CalcButton(props){
-    return <button>{props.value}</button>
+    return <button className={props.className}>{props.value}</button>
 }
